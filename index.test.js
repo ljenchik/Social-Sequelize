73cf70b735289b3185dfe7167e1b7415388533f4
ljenchik <pelagenkolena@gmail.com>
2023-11-02 21:20:33 +0000

    Added many to many tests for users-likes association

--- index.test.js
+++ index.test.js
@@ -18,7 +18,7 @@ describe("Social Sequelzie Test", () => {
         await Profile.bulkCreate(profiles);
         await Post.bulkCreate(posts);
         await Comment.bulkCreate(comments);
-        await Like.bulkCreate(likes);
+        const allLikes = await Like.bulkCreate(likes);
     });
 
     // User-Profile
@@ -80,6 +80,33 @@ describe("Social Sequelzie Test", () => {
             let test = await userWithPosts.getPosts();
             expect(test[0] instanceof Post).toBe(true);
             expect(test[1] instanceof Post).toBe(true);
+        });
+    });
+
+    // User-Like
+    describe("tests many to many users-likes association", () => {
+        test("user associates with many likes", async () => {
+            const foundUser = await User.findOne({
+                where: { username: "alice_wonderland" },
+            });
+            const like1 = await Like.findByPk(1);
+            const like2 = await Like.findByPk(2);
+
+            await foundUser.addLikes([like1, like2]);
+            const userLikes = await foundUser.getLikes();
+            expect(userLikes.length).toBe(2);
+            expect(userLikes[0] instanceof Like).toBe(true);
+        });
+
+        test("like associates with many users", async () => {
+            const foundLike = await Like.findByPk(3);
+            const user1 = await User.findByPk(1);
+            const user2 = await User.findByPk(2);
+
+            await foundLike.addUsers([user1, user2]);
+            const likeUsers = await foundLike.getUsers();
+            expect(likeUsers.length).toBe(2);
+            expect(likeUsers[0] instanceof User).toBe(true);
         });
     });
 });
